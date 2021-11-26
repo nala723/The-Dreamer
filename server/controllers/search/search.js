dc161903b0a7813f062d52f3dba7fa30c336b935
nala723 <enda11@naver.com>
2021-11-26 13:32:12 +0900

    ♿️ Test: 서버 get 요청 수정

--- server/controllers/search/search.js
+++ server/controllers/search/search.js
@@ -20,9 +20,21 @@ module.exports = async (req, res) => {
       );
       return res.status(200).json(sarchRes.data);
     } catch (error) {
-        
-        res.status(response.statusCode).end();
-        console.log('Error', error.message, response.statusCode);
-        console.log(error.config);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
     }
 }   
