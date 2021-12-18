3ebf24450651b6641f2e5ca1efd4dc8a571603af
nala723 <enda11@naver.com>
2021-12-18 17:25:47 +0900

    ✨ Feat: 서버 - mypage - 내 그림들 get controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -14,6 +14,7 @@ module.exports = {
     updateUserInfo: require("./mypage/updateUserInfo"),
     getUserInfo: require("./mypage/getUserInfo"),
     getFavorites: require("./mypage/getFavorites"),
+    getMyPics: require("./mypage/getMyPics"),
     /*-----------------------------------------------------------------*/
     createFavorites: require("./search/createFavorites"),
     cancelFavorites: require("./search/cancelFavorites"),
--- server/routers/mypage.js
+++ server/routers/mypage.js
@@ -6,5 +6,6 @@ const upload = require('../middleware/upload');
 router.patch("/user-info", upload.single('profile'), controllers.updateUserInfo);
 router.get("/user-info", controllers.getUserInfo);
 router.get("/like", controllers.getFavorites);
+router.get("/mypics", controllers.getMyPics);
 
 module.exports = router;
